c9279b3f6692775a9ecbd15b8381b23549f70cfa
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2020-04-14 12:35:15 +0200

    Add files via upload
    Gestating the child

--- myBaby.py
+++ myBaby.py
@@ -12,14 +12,14 @@ def sigmoid_deriv(x):
 	return sigmoid(x) * (1 - sigmoid(x))
 
 def forward(x, w1, w2, predict = False):
-	bias = np.ones((np.shape(x)[0], 1))		# np.shape(X)[0] nos da el número de observaciones
+	#bias = np.ones((np.shape(x)[0], 1))		# np.shape(X)[0] nos da el número de observaciones
 	#x = np.concatenate((bias, x), axis = 1)
-z1 = np.dot(x,w1)
-a1 = sigmoid(z1)
-bias = np.ones((np.shape(a1)[0], 1))
-#a1 = np.concatenate((bias, a1), axis = 1)
-z2 = np.dot(a1,w2)
-a2 = sigmoid(z2)
+	z1 = np.dot(x,w1)
+	a1 = sigmoid(z1)
+	#bias = np.ones((np.shape(a1)[0], 1))
+	#a1 = np.concatenate((bias, a1), axis = 1)
+	z2 = np.dot(a1,w2)
+	a2 = sigmoid(z2)
 	return a2
 
 def cost(a2):
@@ -29,13 +29,13 @@ def cost(a2):
 	cost = np.sum(loss)/m
 	return(cost)
 
-def backprop(m):
+def backprop(m,a2):
 	db2 = 2*(a2-y[m]) * sigmoid_deriv(z2)
 	dw2 = db2 * a2
 	db1 = (db2 * w2.T * sigmoid_deriv(z1)).T
-	dw1 = np.dot(db1, X)
+	dw1 = np.dot(db1, X[m][np.newaxis])
 
-#X[0][np.newaxis]
+#X[n][np.newaxis]
 	return(dw1,dw2,db1,db2)
 
 #====================================================================================================
@@ -52,11 +52,20 @@ y = np.array([[1], [1], [0], [0]])
 w1 = np.random.randn(np.shape(X)[1] , 5) #np.shape(X)[1] nos da el número de variables en cada observación (+1 por bias?), 5 vendrá determinado por neurons
 w2 = np.random.randn(5, 1) #6 es el 5 de neurons + la columna de bias
 
+lr = 0.05
+epochs = 12
+
 #====================================================================================================
 
-predicts = forward(X,w1,w2)
-print(predicts)
 
-print(cost(predicts))
+
+for x in range(epochs):
+	print("==== Generation " + x + " ==========")
+	predicts = forward(X,w1,w2)
+	print(predicts)
+	print(cost(predicts))
+
+	w1 = w1 - lr * dw1
+	w2 = w2 - lr * dw2
 
 
