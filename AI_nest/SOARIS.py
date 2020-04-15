1b109d7daee7ee795afe94344aa1a1a61bb8fc85
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2020-04-15 14:41:39 +0200

    Conception of SOARIS

---
+++ AI_nest/SOARIS.py
@@ -0,0 +1,6 @@
+'''
+This creature, named the Self-Organizing ARtificial Intelligent System, or SOARIS, will born to soar into the heights of artificial intelligence.
+Its architecture is meant to provide a substrate where neural elements grow, develop and reproduce by themselves.
+The intelligence of SOARIS is in continuous and autonomous development.
+The main goal is to create and research artificial consciousness
+'''
