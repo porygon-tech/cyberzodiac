762974f797f5cf1582563ef79535877cbdd2c61c
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2022-10-02 12:54:54 +0200

    Delete SOARIS.py

--- AI_nest/SOARIS.py
+++
@@ -1,6 +0,0 @@
-'''
-This creature, named the Self-Organizing ARtificial Intelligent System, or SOARIS, will be born to soar into the heights of artificial intelligence.
-Its architecture is meant to provide a substrate where neural elements grow, develop and reproduce by themselves.
-The intelligence of SOARIS is in continuous and autonomous development.
-The main goal is to create and research artificial consciousness
-'''
